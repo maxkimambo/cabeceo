12bb753b16e368f0942d59109dabcbdeac5ebc3e
Max Kimambo <max@kimambo.de>
2016-05-22 16:53:21 +0200

    Added update user info endpoint

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -4,8 +4,9 @@ let userController = require('./user.controller');
 let auth = require('./../../auth/auth.service');
 
 // routes for api/users
-
 router.get('/', auth.isAuthenticated(), userController.index);
+
+router.post('/update/:id', userController.update);
 
 // /api/users/   POST
 router.post('/', userController.create);
--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -2,7 +2,9 @@ let config = require('./../../config/environment');
 let User = require('./user.model');
 let jwt = require('jsonwebtoken');
 let authService = require('../../auth/auth.service');
+let log = require('../../components/logger');
 
+const sensitiveData = '-salt -password';
 
 module.exports.index = function(req, res){
     // grab all the users from the db.
@@ -23,9 +25,50 @@ module.exports.create = function(req, res){
 }
 
 module.exports.findById = function(req, res){
-  console.log(req.params.id);
   let userId = req.params.id;
-  User.findOne({_id:userId}).exec().then(user => {
+  // User.findOne({_id:userId}).exec().then(user => {
+  //   res.json(user);
+  // });
+   _findById(userId).then(user => {
     res.json(user);
-  })
+  });
+
+}
+
+// private function to find users by id;
+// return a promise
+function _findById(id){
+    return User.findOne({_id:id}, sensitiveData).exec();
+}
+
+function _findRawById(id){
+  return User.findOne({_id: id}).exec();
+}
+
+module.exports.update = function(req, res){
+  console.log('got here');
+
+   let userId = req.params.id;
+   let newUser = req.body;
+
+   console.log(userId);
+   console.log(newUser);
+
+
+   _findRawById(userId).then(user => {
+       // loop over keys and update
+      //  Object.keys(newUser).forEach(k => {
+      //    user[k] = newUser[k];
+      //  });
+    let userToSave = Object.assign(user, newUser);
+
+    res.json({result: userToSave });
+     // save
+     userToSave.save().then(err => {
+       log.error(err);
+     });
+
+   });
+
+
 }
