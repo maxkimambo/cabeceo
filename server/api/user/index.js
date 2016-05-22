62902b226d887c0a44570331d316e79751913531
Max Kimambo <max@kimambo.de>
2016-05-22 14:19:24 +0200

    Authenticates based on the issued token

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -3,6 +3,6 @@ let log = require('./../../components/logger');
 let userController = require('./user.controller');
 let auth = require('./../../auth/auth.service');
 
-router.get('/', userController.index);
+router.get('/', auth.isAuthenticated(), userController.index);
 
 module.exports = router;
